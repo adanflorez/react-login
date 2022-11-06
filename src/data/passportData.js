6d37793a9817a00a0bc0e070fa5338a00a29bc18
Adan Florez <adan.florezb@gmail.com>
2022-11-06 12:52:50 -0500

    feat: modes

--- src/data/passportData.js
+++ src/data/passportData.js
@@ -1,21 +1,75 @@
 const passportData = [
   {
-    id: 1,
+    id: "1",
     title: "Rio Amazonas",
     image: "/assets/img/amazonas.jpeg",
     description: "Esta es la descripcion",
+    modes: [
+      {
+        title: "Correria",
+        description: "Descripcion del modo",
+      },
+      {
+        title: "Expedicion",
+        description: "Descripcion del modo",
+      },
+      {
+        title: "Aventura",
+        description: "Descripcion del modo",
+      },
+      {
+        title: "Travesia",
+        description: "Descripcion del modo",
+      },
+    ],
   },
   {
-    id: 2,
+    id: "2",
     title: "Rio Orinoco",
     image: "/assets/img/orinoco.jpeg",
     description: "Esta es la descripcion",
+    modes: [
+      {
+        title: "Correria",
+        description: "Descripcion del modo",
+      },
+      {
+        title: "Expedicion",
+        description: "Descripcion del modo",
+      },
+      {
+        title: "Aventura",
+        description: "Descripcion del modo",
+      },
+      {
+        title: "Travesia",
+        description: "Descripcion del modo",
+      },
+    ],
   },
   {
-    id: 3,
+    id: "3",
     title: "Rio Meta",
     image: "/assets/img/meta.jpeg",
     description: "Esta es la descripcion",
+    modes: [
+      {
+        title: "Correria",
+        description: "Descripcion del modo",
+      },
+      {
+        title: "Expedicion",
+        description: "Descripcion del modo",
+      },
+      {
+        title: "Aventura",
+        description: "Descripcion del modo",
+      },
+      {
+        title: "Travesia",
+        description: "Descripcion del modo",
+      },
+    ],
   },
 ];
 
